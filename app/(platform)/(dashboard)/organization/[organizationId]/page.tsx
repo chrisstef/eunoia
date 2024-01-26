f38954b56b7e25da61fe344d15dfcdd5b1c828f2
chrisstef <xristostefanis@gmail.com>
2024-01-27 00:42:36 +0200

    create Server actions/abstractions & form components to implement them

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,19 @@
-const page = () => {
-    return <div>Organization Page </div>;
+import { db } from "@/lib/db";
+import { Board } from "./board";
+import { Form } from "./form";
+
+const page = async () => {
+    const boards = await db.board.findMany();
+    return (
+        <div className="flex flex-col space-y-4">
+            <Form />
+            <div className="space-y-2">
+                {boards.map((board: any) => (
+                    <Board title={board.title} key={board.id} id={board.id} />
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default page;
