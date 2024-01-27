827b0c2652987b7f486135a19239ff02a9f2227a
chrisstef <xristostefanis@gmail.com>
2024-01-27 17:33:00 +0200

    create List component

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -2,6 +2,7 @@ import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 import { db } from "@/lib/db";
+import { ListContainer } from "./_components/list-container";
 
 // import { ListContainer } from "./_components/list-container";
 
@@ -18,31 +19,28 @@ const BoardIdPage = async ({ params }: BoardIdPageProps) => {
         redirect("/select-org");
     }
 
-    //   const lists = await db.list.findMany({
-    //     where: {
-    //       boardId: params.boardId,
-    //       board: {
-    //         orgId,
-    //       },
-    //     },
-    //     include: {
-    //       cards: {
-    //         orderBy: {
-    //           order: "asc",
-    //         },
-    //       },
-    //     },
-    //     orderBy: {
-    //       order: "asc",
-    //     },
-    //   });
+    const lists = await db.list.findMany({
+        where: {
+            boardId: params.boardId,
+            board: {
+                orgId,
+            },
+        },
+        include: {
+            cards: {
+                orderBy: {
+                    order: "asc",
+                },
+            },
+        },
+        orderBy: {
+            order: "asc",
+        },
+    });
 
     return (
         <div className="p-4 h-full overflow-x-auto">
-            {/* <ListContainer
-        boardId={params.boardId}
-        data={lists}
-      /> */}
+            <ListContainer boardId={params.boardId} data={lists} />
             Hello: {orgId}
         </div>
     );
